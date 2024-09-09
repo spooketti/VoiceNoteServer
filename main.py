4ec627cff84bed5a29b00de3fe920722741a2a3c
spooketti <jonliu168@gmail.com>
2024-09-09 10:14:16 -0700

    speech notes

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ def home():
 @app.route("/recieveAudio/",methods=["POST"])
 def recieveAudio():
     audioFile = request.files["why"]
-    speechNoteClass.speechToText(audioFile)
-    speechNoteClass.update_text()
+    speechNoteClass.speechToTextWithNotes(audioFile)
     return "stuff"
 
 
